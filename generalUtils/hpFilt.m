f26ea4c268c0fd170e8a266687962cff629e052a
nsteinme <nick.steinmetz@gmail.com>
2016-01-27 19:06:31 +0000

    add reSVD, fix hpFilt

--- generalUtils/hpFilt.m
+++ generalUtils/hpFilt.m
@@ -1,11 +1,10 @@
 
-function [filtV, vars] = hpFilt(V, Fs, cutoffFreq)
-% function [filtV, vars] = hpFilt(V, Fs, cutoffFreq)
+function filtV = hpFilt(V, Fs, cutoffFreq)
+% function filtV = hpFilt(V, Fs, cutoffFreq)
 % recommended cutoffFreq = 0.01 (Hz). 
 
 order = 3;
-Wn = cutoffFreq/Fs/2;
+Wn = cutoffFreq/(Fs/2);
 [b,a]=butter(order, Wn, 'high');
 
 filtV = single(filtfilt(b,a,double(V')))';
-vars = [];
