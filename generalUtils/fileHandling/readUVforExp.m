85d92b0578cbf95c916dc7cceb01f54d7ab7a13b
Nick Steinmetz <nick@Nicks-MacBook-Pro-2.local>
2016-02-01 19:42:15 +0000

    added reading/writing to NPY

---
+++ generalUtils/fileHandling/readUVforExp.m
@@ -0,0 +1,17 @@
+
+function [U, V, t] = readUVforExp(mouseName, thisDate, expNum, varargin)
+% function [U, V, t] = readUVforExp(mouseName, thisDate, expNum[, nSVtoRead])
+% Returns the U, V, and t (the time indices in Timeline coordinates for a 
+% given mouse, date, and expNum. Optionally reads only a subset of SVs.
+
+
+filePath = dat.expPath(mouseName, thisDate, 1, 'widefield', 'master');
+Upath = fileparts(filePath);
+
+U = readUfromNPY(fullfile(Upath, 'SVD_Results_U.npy'), varargin);
+
+vFilePath = fullfile([dat.expFilePath(mouseName, thisDate, expNum, 'calcium-widefield-svd', 'master') '_V.npy']);
+tFilePath = fullfile([dat.expFilePath(mouseName, thisDate, expNum, 'calcium-widefield-svd', 'master') '_t.npy']);
+
+V = readVfromNPY(vFilePath, varargin);
+t = readNPY(tFilePath);
---
+++ generalUtils/fileHandling/readUfromNPY.m
@@ -0,0 +1,30 @@
+
+
+function readUfromNPY(uFilePath, varargin)
+% function readVfromNPY(uFilePath[, nSVtoRead])
+% Reads a U matrix from an NPY file, optionally reads just a certain number
+% of components
+% Expects that U has shape yPix x xPix x nTimePoints
+
+if isempty(varargin)
+    U = readNPY(vFilePath);
+else
+    nSVtoRead = varargin{1};
+    [arrayShape, dataType, fortranOrder, littleEndian, totalHeaderLength, npyVersion] = readNPYheader(vFilePath);
+    
+    yPix = arrayShape(1);
+    xPix = arrayShape(1);
+    
+    fid = fopen(vFilePath, 'r');
+    try
+        header = fread(fid, totalHeaderLength, '*char');
+        U = fread(fid, yPix*xPix*nSVtoRead, ['*' dataType]);
+        fclose(fid);
+    catch me
+        fclose(fid);
+        rethrow(me);
+    end
+        
+    U = reshape(U, yPix, xPix, nSVtoRead);
+end
+        
