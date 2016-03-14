292187c815590d3c1c2e69bfd9f374839919cb8b
nsteinme <nick.steinmetz@gmail.com>
2016-03-14 17:33:40 +0000

    first version of listener

---
+++ +svdVid/listen.m
@@ -0,0 +1,16 @@
+
+
+function s = listen()
+
+% start the svdVid object
+s = svdVideoObject('-');
+
+% Now setup the UDP communication - code from +et, Krumin
+
+% The Remote Host IP doesn't really matter here, it is just a placeholder
+% For bidirectional communication echo will be sent to the IP the command
+% was received from
+fakeIP = '1.1.1.1';
+myUDP = udp(fakeIP, 1103, 'LocalPort', 1001);
+fopen(myUDP);
+set(myUDP, 'DatagramReceivedFcn', @(src, evt)svdVid.UDPCallback(src, evt, s));
---
+++ core/svdVideoObj.m
@@ -0,0 +1,153 @@
+
+
+classdef svdVideoObj < handle
+    
+    % Keeps track of experiments that are going on and will perform
+    % appropriate cleanup afterwards, including making an SVD
+    % representation of the data for putting on the server
+    %
+    
+    properties
+        ops 
+    end
+    
+    methods (Static)
+        function s = svdVideoObj(a)
+            % constructor 
+            if nargin>0
+                s = svdVideoObj;
+                
+                s.ops = setSVDParams(); % defaults
+                s.ops.mouseName = 'unknownMouse';
+                s.ops.thisDate = 'unknownDate';
+            end
+            
+            fprintf(1, 'svdVid object initialized\n');
+            
+        end
+        
+    end
+    
+    methods                
+        
+        function addExpDat(this, expDat)            
+            if isempty(this.expDat)
+                this.ops.expDats = expDat;
+                [subjectRef, expDate, ~] = parseExpRef(expDat.expRef);
+                this.ops.mouseName = subjectRef;
+                this.ops.thisDate = expDate;
+                this.ops.localSavePath = fullfile('/mnt/data/svdinput/temp/', this.ops.mouseName, this.ops.thisDate);
+            else
+                this.ops.expDats(end+1) = expDat;
+            end
+        end  
+        
+        function describe(this)
+            fprintf(1, 'SVD Video Object for %s %s\n', this.ops.mouseName, this.ops.thisDate);
+        end
+        
+        function wizard(this)
+            this.describe();
+            
+            nCams = input('How many cameras are you using? [1] ');
+            if isempty(nCams); nCams = 1; end
+            this.ops.nCams = nCams;
+            this.ops.camIDNums = [];
+            
+            vidNum = 1;
+            for n = 1:nCams
+                fprintf(1, '  For camera %d:\n', n);
+                
+                camIDNum = input('  What is the total ID number of this camera? [2] ');
+                if isempty(camIDNum); camIDNum = 2; end
+                this.ops.camIDNums(end+1) = camIDNum;
+                
+                Fs = input('  What is the total frame rate of this camera? [50] ');
+                if isempty(Fs); Fs = 50; end
+                
+                exposureDur = input('  What is the exposure duration of this camera? [19] ');
+                if isempty(exposureDur); exposureDur = 19; end
+                
+                nColors = input('  How many colors on this camera? [1] ');
+                if isempty(nColors); nColors = 1; end
+                
+                if nColors>1
+                    
+                    pattern = input(sprintf('    What is the pattern of colors? [%d] ', 1:nColors));
+                    if isempty(pattern); pattern = 1:nColors; end
+                    
+                    for c = 1:nColors
+                        thisName = input(sprintf('    Name of color %d? [cam%dcolor%d] ', c, camIDNum, c));
+                        if isempty(thisName); thisName = sprintf('cam%dcolor%d', camIDNum, c); end
+                        
+                        this.ops.vids(vidNum).name = thisName;
+                        this.ops.vids(vidNum).fileBase = fullfile('/mnt/data/svdinput/', this.ops.mouseName, this.ops.thisDate, sprintf('cam%d', camIDNum));
+                        this.ops.vids(vidNum).frameMod = [nColors,c-1]; % specifies which frames are these. mod(frameNums,frameMod(1))==frameMod(2);
+                        this.ops.vids(vidNum).rigName = sprintf('bigrig%d', camIDNum);
+                        this.ops.vids(vidNum).Fs = Fs/nColors;
+                        this.ops.vids(vidNum).exposureDur = exposureDur;
+                        vidNum = vidNum+1;
+                    end
+                    
+                else
+                    
+                    this.ops.vids(vidNum).name = sprintf('cam%d', camIDNum);
+                    this.ops.vids(vidNum).fileBase = fullfile('/mnt/data/svdinput/', this.ops.mouseName, this.ops.thisDate, sprintf('cam%d', camIDNum));
+                    this.ops.vids(vidNum).frameMod = [1,0]; % specifies which frames are these. mod(frameNums,frameMod(1))==frameMod(2);
+                    this.ops.vids(vidNum).rigName = sprintf('bigrig%d', camIDNum);
+                    this.ops.vids(vidNum).Fs = Fs;
+                    this.ops.vids(vidNum).exposureDur = exposureDur;
+                    vidNum = vidNum+1;
+                    
+                end                                
+                
+            end
+            
+            hasASCIIstamp = input('Using ASCII stamps? [1] ');
+            if isempty(hasASCIIstamp); hasASCIIstamp = true; end
+            this.ops.hasASCIIstamp = logical(hasASCIIstamp);
+        
+            binning = input('Binning to use (2 means 2x2, etc)? [1] ');
+            if isempty(binning); binning = 1; end
+            this.ops.binning = binning;
+            
+            userName = input('Your name? [] ');
+            if isempty(userName); userName = 'unknownUser'; end
+            this.ops.userName = userName;
+            
+            switch lower(userName)
+                case 'daisuke'
+                    this.ops.emailAddress = 'd.shimaoka@ucl.ac.uk';
+                case 'elina'
+                    this.ops.emailAddress = 'elina.jacobs.13@ucl.ac.uk';
+                case 'mika'
+                    this.ops.emailAddress = 'efthymia.diamanti.11@ucl.ac.uk';
+                case 'nick'
+                    this.ops.emailAddress = 'nick.steinmetz@gmail.com';    
+                otherwise
+                    this.ops.emailAddress = [];
+            end
+        end
+        
+        function go(this)
+            
+            mkdir(fullfile('\\lugaro.cortexlab.net\svdinput\', this.ops.mouseName, this.ops.thisDate));
+            if this.ops.nCams>1
+                for n = 1:this.ops.nCams
+                    mkdir(fullfile('\\lugaro.cortexlab.net\svdinput\', this.ops.mouseName, this.ops.thisDate, sprintf('cam%d', this.ops.camIDNums(n))));
+                end
+            end
+            
+            save(fullfile('\\lugaro.cortexlab.net\svdinput\', this.ops.mouseName, this.ops.thisDate, 'ops.mat'), this.ops);
+            
+            fprintf(1, 'Made directories for you at %s, go copy your files there\n', fullfile('\\lugaro.cortexlab.net\svdinput\', this.ops.mouseName, this.ops.thisDate));                        
+            
+        end
+        
+        
+    end
+end
+
+                
+                
+                
